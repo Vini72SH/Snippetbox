696759e06169eac5e91a828c9e60a69f2b5d7e27
Vinicius <viniciusjere@outlook.com>
2025-10-29 14:53:08 -0300

    Database Insert

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Vini72SH/snippetbox/internal/models"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func openDB(dsn string) (*sql.DB, error) {
@@ -45,7 +48,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("Starting server", "addr", *addr)
