2e6126aafe27d9b5c4d7be0ad504dc9e077c66bb
Vinícius Santos <viniciusjere@outlook.com>
2026-01-07 10:54:51 -0300

    Logger Middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -18,3 +18,20 @@ func commonHeaders(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(fn)
 }
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.URL.RequestURI()
+		)
+
+		app.logger.Info("received request", "ip", ip, "proto", proto, "method", method, "uri", uri)
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
+}
